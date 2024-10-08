77db64cde7c0d5fa0e7a29d8b0ac464edddc787f
kimsh0306 <b666790@gmail.com>
2024-10-08 20:14:58 +0900

    Test: test push

--- index.js
+++ index.js
@@ -14,10 +14,8 @@ app.use(express.json()); // 바디파서
 app.use(express.urlencoded({ extended: true })); // 바디파서
 app.use("/api/users", require("./routes/userRoutes"));
 
-
 // app.listen(5000, () => {
 //   console.log("서버 실행 중");
 // });
-
 
 module.exports = app;
