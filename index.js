aad8b93bb2100961c923830f272b7714f170ab8f
kimsh0306 <b666790@gmail.com>
2024-10-10 18:18:50 +0900

    Add: cors

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const dbConnect = require("./config/dbConnect");
-
+const cors = require('cors');
 const app = express();
 
 dbConnect();
+
+// CORS 설정
+app.use(cors({
+  origin: 'http://localhost:3000', // 허용할 도메인
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // 허용할 HTTP 메서드
+  credentials: true, // 쿠키 전송 허용
+}));
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello, Node!");
@@ -14,8 +23,8 @@ app.use(express.json()); // 바디파서
 app.use(express.urlencoded({ extended: true })); // 바디파서
 app.use("/users", require("./routes/userRoutes"));
 
-// app.listen(5000, () => {
-//   console.log("서버 실행 중");
-// });
+app.listen(5000, () => {
+  console.log("서버 실행 중");
+});
 
 module.exports = app;
