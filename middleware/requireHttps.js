fc2442787f69ef97344e74ef1aa9489a707c20a2
kimsh0306 <b666790@gmail.com>
2024-10-30 15:49:32 +0900

    Fix: x-forwarded-proto로 HTTPS 요청 확인

--- middleware/requireHttps.js
+++ middleware/requireHttps.js
@@ -1,6 +1,9 @@
 const requireHttps = (req, res, next) => {
   // 배포 환경에서만 HTTPS 강제
-  if (process.env.NODE_ENV === "production" && !req.secure) {
+  if (
+    process.env.NODE_ENV === "production" &&
+    req.headers['x-forwarded-proto'] !== 'https'
+  ) {
     return res.status(403).json({ message: "HTTPS 요청만 허용됩니다." });
   }
   next();
